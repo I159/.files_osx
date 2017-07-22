9549e95c462b9e6612ef7793494841f4f03ae4b8
Ilya Pekelny <pekelny@gmail.com>
2017-07-22 22:20:47 +0300

    Move vim to a separate repo

--- install.sh
+++ install.sh
@@ -1,22 +1,8 @@
 DOTFILES="$(pwd)"
-echo -e "Update vim plugins and other funcy staff"
-git submodule update --init --recursive
-
-echo -e "Link colors and autoload to a right places"
-rm "$(pwd)/.vim/colors"
-ln -s "$(pwd)/wombat256.vim/colors" "$(pwd)/.vim/colors"
-rm "$(pwd)/.vim/autoload"
-ln -s "$(pwd)/.vim/bundle/vim-pathogen/autoload" "$(pwd)/.vim/autoload"
 
 echo -e "Backup existing dot files"
 DATE=$(date +%Y-%m-%d-%T)
 mkdir $HOME/.backup_dot_files-$DATE
-if [ -L $HOME/.vimrc ]
-then
-	rm -f $HOME/.vimrc
-else
-	mv -f $HOME/.vimrc $HOME/.backup_dot_files
-fi
 
 if [ -L $HOME/.gitconfig ]
 then
@@ -43,10 +29,3 @@ echo -e "Create symbolic links to dot files"
 ln -s $DOTFILES/.gitconfig.exmpl  $HOME/.gitconfig
 ln -s $DOTFILES/.bashrc $HOME/.bashrc
 ln -s $DOTFILES/.bash_profile $HOME/.bash_profile
-ln -s $DOTFILES/.vimrc $HOME/.vimrc
-rm $HOME/.vim
-ln -s $DOTFILES/.vim $HOME/.vim
-rm $HOME/.vimpy
-ln -s $DOTFILES/.vimpy $HOME/.vimpy
-rm $HOME/.vimgo
-ln -s $DOTFILES/.vimgo $HOME/.vimgo
